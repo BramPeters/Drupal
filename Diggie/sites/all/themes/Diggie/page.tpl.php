5f9a33e75f38542be84411a8e32fafb4a735e01a
Bram <brampeters22@hotmail.com>
2014-05-23 13:39:37 +0200

    Diggie uitbreiding 23-05

---
+++ Diggie/sites/all/themes/Diggie/page.tpl.php
@@ -0,0 +1,297 @@
+<!-- #header -->
+<div id="header">
+	<!-- #header-inside -->
+    <div id="header-inside" class="container_12 clearfix">
+    	<!-- #header-inside-left -->
+        <div id="header-inside-left" class="grid_8">
+            
+            <?php if ($logo): ?>
+            <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+            <?php endif; ?>
+     
+            <?php if ($site_name || $site_slogan): ?>
+            <div class="clearfix">
+            <?php if ($site_name): ?>
+            <span id="site-name"><a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></span>
+            <?php endif; ?>
+            <?php if ($site_slogan): ?>
+            <span id="slogan"><?php print $site_slogan; ?></span>
+            <?php endif; ?>
+            </div>
+            <?php endif; ?>
+            
+        </div><!-- EOF: #header-inside-left -->
+        
+        <!-- #header-inside-right -->    
+        <div id="header-inside-right" class="grid_4">
+
+			<?php print render($page['search_area']); ?>
+
+        </div><!-- EOF: #header-inside-right -->
+    
+    </div><!-- EOF: #header-inside -->
+
+</div><!-- EOF: #header -->
+
+<!-- #header-menu -->
+<div id="header-menu">
+	<!-- #header-menu-inside -->
+    <div id="header-menu-inside" class="container_12 clearfix">
+    
+    	<div class="grid_12">
+            <div id="navigation" class="clearfix">
+            <?php if ($page['navigation']) :?>
+            <?php print drupal_render($page['navigation']); ?>
+            <?php else :
+            if (module_exists('i18n_menu')) {
+            $main_menu_tree = i18n_menu_translated_tree(variable_get('menu_main_links_source', 'main-menu'));
+            } else {
+            $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu')); 
+            }
+            print drupal_render($main_menu_tree);
+            endif; ?>
+            </div>
+        </div>
+        
+    </div><!-- EOF: #header-menu-inside -->
+
+</div><!-- EOF: #header-menu -->
+
+<!-- #banner -->
+<div id="banner">
+
+	<?php print render($page['banner']); ?>
+	
+    <?php if (theme_get_setting('slideshow_display','Diggie')): ?>
+    
+    <?php if ($is_front): ?>
+    
+    <!-- #slideshow -->
+    <div id="slideshow">
+    
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                
+                <!--slider-item content-->
+                <div style="float:left; padding:0 30px 0 0;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'Diggie') ;?>/mockup/slide-1.jpg" style="height:268px"/>
+                </div>
+                <h2>Peter- & Meterschap</h2>
+                <strong>Adopteer een van onze dieren</strong><br/>
+                <em></em><br/>
+                <br/>
+                Op Diggie Kinder- en Gezinsboerderij wonen heel wat grote en kleine dieren. Deze hebben heel wat zorg, liefde en voeding nodig. Ook een droog en proper huisje, voor onze beestjes is onontbeerlijk. Diggie wil uiteraard de beste zorgen blijven aan haar bewoners. Dit alles gebeurt niet gratis en een financieel duwtje in de rug is hierbij altijd welkom.
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="#">Leer meer</a></div>
+                <!--EOF:slider-item content-->
+                <br/>
+                <br/>
+                </div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+        
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                
+                <!--slider-item content-->
+                <div style=" padding:0 0 0 00px;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'Diggie') ;?>/mockup/slide-2.jpg" style="height:168px;"/>
+                </div>
+                <h2>Op kamp gaan naar Diggie</h2>
+                <!-- <strong></strong><br/>
+                <em></em><br/>
+                <br/>-->
+                Diggiekampen zijn er voor iedereen. Voor kleutertjes van drie tot pubers van zestien. We ‘wonen’ een aantal dagen op de Diggiehoeve. We slapen er in stapelbedden, eten in de omgebouwde koestal en brengen de rest van de tijd door op de weide, de binnenkoer of op de hooizolder. Zo nu en dan zetten we een stapje in de wereld en trekken we naar het bos, het zwembad of …
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="#">Tell me more</a></div>
+                <!--EOF:slider-item content-->
+                
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+        
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                <div style=" padding:0 0 0 00px;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'Diggie') ;?>/mockup/slide-3.jpg" style="height:168px;float:left;"/>
+                </div>
+                <!--slider-item content-->
+                <h2>Jeugdverblijf</h2>
+                <strong>General</strong><br/>
+                <em>Client name</em><br/>
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="#">Tell me more</a></div>
+                <!--EOF:slider-item content-->
+                
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+        
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                <div style=" padding:0 0 0 00px;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'Diggie') ;?>/mockup/slide-4.jpg" style="height:168px;float:left;"/>
+                </div>
+                <!--slider-item content-->
+                <h2>Scholen en groepen</h2>
+                <strong>General</strong><br/>
+                <em>Client name</em><br/>
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="#">Tell me more</a></div>
+                <!--EOF:slider-item content-->
+                
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+    
+    </div>
+    <!-- EOF: #slideshow -->
+    
+    <!-- #slider-controls-wrapper -->
+    <div id="slider-controls-wrapper">
+        <div id="slider-controls" class="container_12">
+            <ul id="slider-navigation">
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- EOF: #slider-controls-wrapper -->
+    
+    <?php endif; ?>
+    
+	<?php endif; ?>  
+
+</div><!-- EOF: #banner -->
+
+
+<!-- #content -->
+<div id="content">
+	<!-- #content-inside -->
+    <div id="content-inside" class="container_12 clearfix">
+    
+        <?php if ($page['sidebar_first']) :?>
+        <!-- #sidebar-first -->
+        <div id="sidebar-first" class="grid_4">
+        	<?php print render($page['sidebar_first']); ?>
+        </div><!-- EOF: #sidebar-first -->
+        <?php endif; ?>
+        
+        <?php if ($page['sidebar_first'] && $page['sidebar_second']) { ?>
+        <div class="grid_4">
+        <?php } elseif ($page['sidebar_first'] || $page['sidebar_second']) { ?>
+        <div id="main" class="grid_8">
+		<?php } else { ?>
+        <div id="main" class="grid_12">    
+        <?php } ?>
+            
+            <?php if (theme_get_setting('breadcrumb_display','Diggie')): print $breadcrumb; endif; ?>
+            
+            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+       
+            <?php if ($messages): ?>
+            <div id="console" class="clearfix">
+            <?php print $messages; ?>
+            </div>
+            <?php endif; ?>
+     
+            <?php if ($page['help']): ?>
+            <div id="help">
+            <?php print render($page['help']); ?>
+            </div>
+            <?php endif; ?>
+            
+            <?php if ($action_links): ?>
+            <ul class="action-links">
+            <?php print render($action_links); ?>
+            </ul>
+            <?php endif; ?>
+            
+			<?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+            <h1><?php print $title ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            
+            <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
+            
+            <?php print render($page['content']); ?>
+            
+            <?php print $feed_icons; ?>
+            
+        </div><!-- EOF: #main -->
+        
+        <?php if ($page['sidebar_second']) :?>
+        <!-- #sidebar-second -->
+        <div id="sidebar-second" class="grid_4">
+        	<?php print render($page['sidebar_second']); ?>
+        </div><!-- EOF: #sidebar-second -->
+        <?php endif; ?>  
+
+    </div><!-- EOF: #content-inside -->
+
+</div><!-- EOF: #content -->
+
+<!-- #footer -->    
+<div id="footer">
+	<!-- #footer-inside -->
+    <div id="footer-inside" class="container_12 clearfix">
+    
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_first']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_second']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_third']); ?>
+        </div><!-- EOF: .footer-area -->
+       
+    </div><!-- EOF: #footer-inside -->
+
+</div><!-- EOF: #footer -->
+
+<!-- #footer-bottom -->    
+<div id="footer-bottom">
+
+	<!-- #footer-bottom-inside --> 
+    <div id="footer-bottom-inside" class="container_12 clearfix">
+    	<!-- #footer-bottom-left --> 
+    	<div id="footer-bottom-left" class="grid_8">
+        
+            <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('secondary-menu', 'links', 'clearfix')))); ?>
+            
+            <?php print render($page['footer']); ?>
+            
+        </div>
+    	<!-- #footer-bottom-right --> 
+        <div id="footer-bottom-right" class="grid_4">
+        
+        	<?php print render($page['footer_bottom_right']); ?>
+        
+        </div><!-- EOF: #footer-bottom-right -->
+       
+    </div><!-- EOF: #footer-bottom-inside -->
+    
+    <!-- #credits -->   
+    <div id="credits" class="container_12 clearfix">
+    </div>
+    <!-- EOF: #credits -->
+
+</div><!-- EOF: #footer -->
